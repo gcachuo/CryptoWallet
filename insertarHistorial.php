0a9eaabce685759d011533f6d3d4bf6cacbfc1cf
cachu <gcachu.o@gmail.com>
2017-01-12 17:36:28 -0600

    request

--- insertarHistorial.php
+++ insertarHistorial.php
@@ -6,6 +6,6 @@
  * Time: 04:25 PM
  */
 
-$file_data = "$_POST[cash]\n";
+$file_data = "$_REQUEST[cash]\n";
 $file_data .= file_get_contents('historial.txt');
 file_put_contents('historial.txt', $file_data);
---
+++ maker.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Memo
+ * Date: 12/ene/2017
+ * Time: 05:30 PM
+ */
+
