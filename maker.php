07c9ccd68c28e38b6196e7488c49c2e3b54e1e1b
cachu <gcachu.o@gmail.com>
2017-01-13 13:51:59 -0600

    maker + localbitcoins

--- maker.php
+++ maker.php
@@ -6,13 +6,15 @@
  * Time: 05:30 PM
  */
 include "keys.php";
+include "localbitcoins.php";
 $ticker = request("https://api.bitso.com/v2/ticker");
 $balance = request("https://api.bitso.com/v2/balance", array("key" => $key, "nonce" => $nonce, "signature" => $signature));
 
 $bid = $ticker->bid - ($ticker->bid * 0.01);
 $mxn = round($balance->btc_available * $bid, 2);
+$local = round($localbitcoins * $balance->btc_available, 2);
 
-request("https://maker.ifttt.com/trigger/bitcoin/with/key/chImOTt-BFhD5zcj3BzzOz", array("value1" => "$mxn"));
+request("https://maker.ifttt.com/trigger/bitcoin/with/key/chImOTt-BFhD5zcj3BzzOz", array("value1" => "$mxn | $localbitcoins"));
 
 $file_data = "$mxn\n";
 $file_data .= file_get_contents('historial.txt');
