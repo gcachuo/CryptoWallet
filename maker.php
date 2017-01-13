c378a71c04da12448c455b63a16ffef77878063b
cachu <gcachu.o@gmail.com>
2017-01-12 18:02:13 -0600

    ifttt

--- maker.php
+++ maker.php
@@ -5,4 +5,30 @@
  * Date: 12/ene/2017
  * Time: 05:30 PM
  */
+include "keys.php";
+$ticker = request("https://api.bitso.com/v2/ticker");
+$balance = request("https://api.bitso.com/v2/balance", array("key" => $key, "nonce" => $nonce, "signature" => $signature));
 
+$bid = $ticker->bid - ($ticker->bid * 0.01);
+$mxn = round($balance->btc_available * $bid, 2);
+
+request("https://maker.ifttt.com/trigger/bitcoin/with/key/chImOTt-BFhD5zcj3BzzOz", array("value1" => "$mxn"));
+
+echo $mxn;
+
+function request($url, $data = array())
+{
+// use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */
+    }
+    return json_decode($result);
+}
