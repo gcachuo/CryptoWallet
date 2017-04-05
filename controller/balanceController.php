fbb17f83482510e1a5ff2b013963763aaaab212f
cachu <gcachu.o@gmail.com>
2017-04-05 16:53:27 -0500

    operations

--- controller/balanceController.php
+++ controller/balanceController.php
@@ -19,7 +19,8 @@ class balanceController extends tickerController
         $eth_btc,
         $objectiveBid,
         $objectiveAsk,
-        $total;
+        $totalBtc,
+        $totalEth;
 
     function __construct()
     {
@@ -27,13 +28,13 @@ class balanceController extends tickerController
         $this->setBalance();
 
         Config::$objective = round(isset($_GET['o']) ? $_GET['o'] : 6270, 2);
-        Config::$objectiveBitcoinFix = isset($_GET['b']) ? $_GET['b'] : 0.0307;
+        Config::$objectiveBitcoinFix = (isset($_GET['b']) ? $_GET['b'] : 0.0307) * 1.02;
         Config::$plusFee = 1 + ($this->balance->fee / 100);
         Config::$minusFee = 1 - ($this->balance->fee / 100);
 
         $this->btc_mxn = round(($this->balance->mxn_balance + Config::$plusWithdraw) / ($this->ticker->btc_mxn->ask * Config::$minusFee), 8);
 
-        $this->btc_eth = round(($this->balance->mxn_balance * ($this->ticker->eth_mxn->ask * Config::$minusFee)) / ($this->balance->mxn_balance * $this->ticker->btc_mxn->ask * Config::$minusFee), 8);
+        $this->btc_eth = ($this->balance->eth_balance * ($this->ticker->eth_mxn->ask * Config::$plusFee)) / ($this->ticker->btc_mxn->ask * Config::$plusFee);
 
         $this->mxn_btc = round($this->balance->btc_balance * ($this->ticker->btc_mxn->ask * Config::$plusFee), 2);
 
@@ -41,12 +42,13 @@ class balanceController extends tickerController
 
         $this->eth_mxn = round(($this->balance->mxn_balance + Config::$plusWithdraw) / ($this->ticker->eth_mxn->ask * Config::$minusFee), 8);
 
-        $this->eth_btc = round(($this->balance->mxn_balance * ($this->ticker->btc_mxn->ask * Config::$minusFee)) / ($this->balance->mxn_balance * $this->ticker->eth_mxn->ask * Config::$minusFee), 8);
+        $this->eth_btc = ($this->balance->btc_balance * ($this->ticker->btc_mxn->ask * Config::$plusFee)) / ($this->ticker->eth_mxn->ask * Config::$plusFee);
 
         $this->objectiveBid = round((Config::$objective / $this->balance->btc_balance) * Config::$plusFee, 2);
         $this->objectiveAsk = round((($this->balance->mxn_balance + Config::$plusWithdraw) / (Config::$objectiveBitcoinFix * Config::$plusFee)), 2);
 
-        $this->total = round($this->mxn_btc + $this->balance->mxn_balance, 2);
+        $this->totalBtc = round($this->mxn_btc + $this->balance->mxn_balance, 2);
+        $this->totalEth = round($this->mxn_eth + $this->balance->mxn_balance, 2);
     }
 
     function setBalance()
---
+++ controller/operationsController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Cachu
+ * Date: 05/abr/2017
+ * Time: 04:00 PM
+ */
+class operationsController extends balanceController
+{
+    public $sellBtc, $sellMxnFee, $priceBtc, $btnBuy;
+    public $sellMxn, $sellBtcFee, $priceMxn, $btnSell;
+
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->sellBtc = number_format(round(($this->btc_mxn - Config::$objectiveBitcoinFix), 8), 8);
+
+        $this->sellMxnFee = round(($this->sellBtc * ($this->ticker->btc_mxn->bid * Config::$minusFee)), 2);
+
+        $this->priceBtc = round($this->ticker->btc_mxn->bid * Config::$minusFee, 2);
+
+        Config::generateSignature($nonce, $signature);
+        $key = Config::$key;
+        if ($this->sellMxnFee > 0) {
+            $this->btnBuy = <<<HTML
+<button class="btn btn-default" onclick="buy({$this->sellBtc},$this->priceBtc,'$key','$nonce','$signature')">Buy</button>
+HTML;
+        }
+        $this->sellMxn = $this->mxn_btc - Config::$objective;
+        $this->sellBtcFee = $this->sellMxn / ($this->ticker->btc_mxn->ask * Config::$plusFee);
+        $this->priceMxn = $this->ticker->btc_mxn->ask * Config::$plusFee;
+    }
+}
