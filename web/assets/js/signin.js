3bec6960975319f04b6be27085e454a75d31a1d8
Memo Cachú <gcachu.o@gmail.com>
2020-02-07 01:45:16 -0600

    signIn

--- web/assets/js/signin.js
+++ web/assets/js/signin.js
@@ -1,7 +1,35 @@
 $(function () {
-    $("form").submit(function(e){
+    localStorage.removeItem('user');
+    $("form").submit(function (e) {
         e.preventDefault();
-        toastr.info('true');
+
+        var data = $(e.currentTarget).serializeArray();
+
+        $.post("api/users/signIn", data).done(({status, code, response: {message, data: {user}}, error}) => {
+            localStorage.setItem('user', JSON.stringify(user));
+            location.href = 'cartera';
+        }).fail(response => {
+            if (response.responseJSON) {
+                const {status, code, response: {message}} = response.responseJSON;
+                switch (true) {
+                    case code >= 500:
+                        toastr.error('An error ocurred.');
+                        console.error(error_message, response.responseJSON);
+                        break;
+                    case code >= 400:
+                        toastr.warning(message);
+                        console.warn(message);
+                        return;
+                    default:
+                        toastr.error('An error ocurred.');
+                        console.error(response.responseJSON);
+                        break;
+                }
+            } else if (response.responseText) {
+                toastr.error('An error ocurred.');
+                console.error(`${response.responseText}`);
+            }
+        });
 
         return false;
     });
