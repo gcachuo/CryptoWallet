5f38d24349eb51b99de9c0194b2861f2cf0f779c
Memo Cachú <gcachu.o@gmail.com>
2020-02-25 18:32:28 -0600

    user

--- web/assets/js/cartera/new.js
+++ web/assets/js/cartera/new.js
@@ -2,6 +2,8 @@ $(function () {
         if (!JSON.parse(localStorage.getItem('user'))) {
             location.href = 'login';
             return;
+        } else {
+            const user = JSON.parse(localStorage.getItem('user'));
         }
     }
 );
