58ce03c90493c86229851837e89ca671e6641116
Memo Cachú <gcachu.o@gmail.com>
2021-11-12 18:07:17 -0600

    fees

--- api/Model/Usuarios_Transacciones.php
+++ api/Model/Usuarios_Transacciones.php
@@ -144,7 +144,7 @@ sql;
         $sql = <<<sql
 UPDATE usuarios_transacciones
 SET precio_real_usuario_moneda     = IF(id_moneda = 'mxn', 1,
-                                        IF(cantidad_usuario_moneda != 0, costo_usuario_moneda / cantidad_usuario_moneda,
+                                        IF(cantidad_usuario_moneda != 0, (costo_usuario_moneda / cantidad_usuario_moneda)*(1-(0.65/100)),
                                            0))
 WHERE precio_real_usuario_moneda IS NULL;
 sql;
