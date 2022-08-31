ab9a664f6d893a854a90a5eab7fb0062a13c7b5b
Memo Cachú <gcachu.o@gmail.com>
2022-08-31 03:30:16 -0500

    update submodules

--- web/ajax/save-token.php
+++ web/ajax/save-token.php
@@ -2,6 +2,6 @@
 include_once __DIR__ . '/../core/System.php';
 System::init(['DIR' => __DIR__ . '/..', 'ENV' => 'www']);
 System::check_value_empty($_GET, ['token']);
-System::sessionSet('user_token', $_GET['token']);
 $token = $_GET['token'];
+System::sessionSet('user_token', $token);
 JsonResponse::sendResponse('Completed', compact('token'));
