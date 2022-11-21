fe3c879046673eb0f406c9e1abe64cdb3e28b8ba
Memo Cachú <gcachu.o@gmail.com>
2022-11-21 14:39:56 -0600

    Add react navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Cartera from "./Screens/Cartera";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Cartera" component={Cartera} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/Screens/Cartera/index.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function Cartera(){
+    return <View><Text>Test</Text></View>;
+}
